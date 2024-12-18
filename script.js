fd559fe571bbd56cbdbe9f8eec9e9a539866a69a
madina2009xon <mahkamboyevamadina2@gmail.com>
2024-12-18 20:06:36 +0300

    lampochka

---
+++ script.js
@@ -0,0 +1,10 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let rasm = document.getElementById("rasm");
+
+function yoqish(){
+    rasm.setAttribute("src","yoniq.png");
+}
+function ochirish(){
+    rasm.setAttribute("src","ochiq.png");
+}
